a8f41b42c4ef9bc0477f4cef10162a4407330d0c
vaporvee <yannik@vaporvee.com>
2024-04-18 00:40:40 +0200

    added camera

--- project/scenes/player.gd
+++ project/scenes/player.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
 class_name Player
 
-var session_id: String
+var session_id: String#
+var is_current_player: bool
 
 func _enter_tree() -> void:
 	$NameTag.text = session_id
+
+func make_current() -> void:
+	$Camera2D.enabled = true
+	is_current_player = true
--- project/scenes/world.gd
+++ project/scenes/world.gd
@@ -6,16 +6,18 @@ func _ready():
 	UdpClient.player_disconnect.connect(_on_player_disconnect)
 
 func _on_player_join(player_client: Dictionary, clients: Array) -> void:
-	add_player(player_client)
+	add_player(player_client, true)
 	for client in clients:
-		add_player(client)
+		add_player(client, false)
 
 
-func add_player(client: Dictionary):
+func add_player(client: Dictionary, is_current_player: bool):
 	var player: Player = preload("res://scenes/player.tscn").instantiate()
 	player.session_id = client.session_id
 	player.position = Vector2(client.position.x, client.position.y)
 	add_child(player)
+	if is_current_player:
+		player.make_current()
 
 func _on_button_pressed():
 	var payload: Dictionary = {
